ef98e6a807f35d0af1e817cab4990a1a677fb67c
rupam089 <152879171+rupam089@users.noreply.github.com>
2023-12-04 19:27:37 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, jsonify
+from werkzeug.utils import secure_filename
+import os
+
+app = Flask(__name__)
+
+# Set the upload folder
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'myFile' not in request.files:
+        return jsonify({'error': 'No file part'})
+
+    file = request.files['myFile']
+
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'})
+
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(filepath)
+        return jsonify({'image': filename})
+    else:
+        return jsonify({'error': 'Invalid file format'})
+
+if __name__ == '__main__':
+    if not os.path.exists(UPLOAD_FOLDER):
+        os.makedirs(UPLOAD_FOLDER)
+    app.run(host='0.0.0.0', port=5000, debug=True)
